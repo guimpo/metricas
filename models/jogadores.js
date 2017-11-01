148ad4db51aa2064b20a0c932bbd42c8aa19fc1c
guimpo <nakaima.prk@gmail.com>
2017-11-01 15:35:50 -0200

    Modificado  tipo do cep para string

--- models/jogadores.js
+++ models/jogadores.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING
     },
     cep: {
-      type: DataTypes.INTEGER
+      type: DataTypes.STRING
     }
     }, {
       tableName: "Jogadores"
